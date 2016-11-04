1fa2481df42d9bc3ff5fdd131dbf3d41f6a865cd
Alexander Brand <abrand@apprenda.com>
2016-11-04 16:34:28 -0400

    Don't print help / cobra error message if kuberang fails

--- cmd/cli.go
+++ cmd/cli.go
@@ -8,13 +8,15 @@ import (
 )
 
 // NewKismaticCommand creates the kismatic command
-func NewKuberangCommand(version string, in io.Reader, out io.Writer) (*cobra.Command) {
+func NewKuberangCommand(version string, in io.Reader, out io.Writer) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "kuberang",
 		Short: "kuberang tests your kubernetes cluster using kubectl",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			return doCheckKubernetes(args)
 		},
+		SilenceUsage:  true,
+		SilenceErrors: true,
 	}
 	return cmd
 }
